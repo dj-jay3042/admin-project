bf56f16ba4053fee5a3d48235420826a11bfd008
dj-jay3042 <cjay3042002@gmail.com>
2023-06-14 16:41:26 +0530

    update

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './App.vue';
+// import FormFile from './Form.vue'
 
-createApp(App).mount('#app')
+createApp(App).mount('#form')
+// createApp(FormFile).mount('#form')
