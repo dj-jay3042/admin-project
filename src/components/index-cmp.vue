c6f63cefaa8ada06c82265729c937445ae04274d
dj-jay3042 <cjay3042002@gmail.com>
2023-06-14 18:58:41 +0530

    new

--- src/components/index-cmp.vue
+++ src/components/index-cmp.vue
@@ -25,6 +25,81 @@
           <!-- /.container-fluid -->
         </div>
         <!-- /.content-header -->
+        <!-- Main content -->
+        <section class="content">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-lg-6">
+                <!-- small box -->
+                <div class="small-box bg-success">
+                  <div class="inner">
+                    <h3>{{ time }}</h3>
+
+                    <p>New Orders</p>
+                  </div>
+                  <div class="icon">
+                    <i class="ion ion-clock"></i>
+                  </div>
+                  <a href="#" class="small-box-footer"
+                    >More info <i class="fas fa-arrow-circle-right"></i
+                  ></a>
+                </div>
+              </div>
+              <div class="col-lg-6">
+                <!-- small box -->
+                <div class="small-box bg-danger">
+                  <div class="inner">
+                    <h3>{{ Time }}</h3>
+
+                    <p>New Orders</p>
+                  </div>
+                  <div class="icon">
+                    <i class="ion ion-clock"></i>
+                  </div>
+                  <a href="#" class="small-box-footer"
+                    >More info <i class="fas fa-arrow-circle-right"></i
+                  ></a>
+                </div>
+              </div>
+            </div>
+            <!-- Small boxes (Stat box) -->
+            <div class="row">
+              <div class="col-lg-3 col-6">
+                <!-- small box -->
+                <div class="small-box bg-info">
+                  <div class="inner">
+                    <h3>{{ count }}</h3>
+
+                    <p>New Orders</p>
+                  </div>
+                  <div class="icon">
+                    <i class="ion ion-bag"></i>
+                  </div>
+                  <a href="#" class="small-box-footer"
+                    >More info <i class="fas fa-arrow-circle-right"></i
+                  ></a>
+                </div>
+              </div>
+              <div class="col-lg-3 col-6">
+                <button
+                  type="button"
+                  class="btn btn-block bg-gradient-success"
+                  v-on:click="add()"
+                >
+                  Increment
+                </button>
+                <button
+                  type="button"
+                  class="btn btn-block bg-gradient-danger"
+                  v-on:click="remove()"
+                >
+                  Decrement
+                </button>
+              </div>
+            </div>
+            <!-- /.row -->
+          </div>
+        </section>
       </div>
       <footerFile />
     </div>
@@ -38,6 +113,48 @@ import footerFile from "./master/footer-master.vue";
 
 export default {
   name: "indexFile",
+  data() {
+    return {
+      count: 0,
+      Time: "09:30:00",
+      time: new Date().toLocaleTimeString(),
+    };
+  },
+  mounted() {
+    setInterval(() => {
+      this.time = new Date().toLocaleTimeString(undefined, {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        hour12: true,
+      });
+
+      var cTime = new Date();
+      var tTime = new Date(
+        cTime.getFullYear(),
+        cTime.getMonth(),
+        cTime.getDate(),
+        10,
+        7,
+        0
+      );
+
+      var dTime = cTime - tTime;
+      var h = Math.floor(dTime / (1000 * 60 * 60));
+      var m = Math.floor((dTime % (1000 * 60 * 60)) / (1000 * 60));
+      var s = Math.floor((dTime % (1000 * 60)) / 1000);
+      var ms = Math.floor((dTime % (1000)) / 1);
+      this.Time = ((h < 10) ? ("0" + h) : (h)) + ":" + ((m < 10) ? ("0" + m) : (m)) + ":" + ((s < 10) ? ("0" + s) : (s)) + ":" + ms;
+    }, 1);
+  },
+  methods: {
+    add() {
+      this.count++;
+    },
+    remove() {
+      this.count != 0 ? this.count-- : this.count;
+    },
+  },
   components: {
     headerFile,
     sidebarFile,
