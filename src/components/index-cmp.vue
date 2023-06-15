3710a06852f9a87763ce32873f86f8d3593d28be
dj-jay3042 <cjay3042002@gmail.com>
2023-06-15 18:24:39 +0530

    by Jay Chauhan 15/06/2023

--- src/components/index-cmp.vue
+++ src/components/index-cmp.vue
@@ -35,7 +35,7 @@
                                 <div class="inner">
                                     <h3>{{ time }}</h3>
 
-                                    <p>New Orders</p>
+                                    <p>Current Time</p>
                                 </div>
                                 <div class="icon">
                                     <i class="ion ion-clock"></i>
@@ -49,7 +49,7 @@
                                 <div class="inner">
                                     <h3>{{ Time }}</h3>
 
-                                    <p>New Orders</p>
+                                    <p>Worked Hours</p>
                                 </div>
                                 <div class="icon">
                                     <i class="ion ion-clock"></i>
@@ -75,15 +75,64 @@
                             </div>
                         </div>
                         <div class="col-lg-3 col-6">
-                            <button type="button" class="btn btn-block bg-gradient-success" v-on:click="add()">
+                            <button type="button" class="btn btn-block bg-gradient-success" v-on:click="increment()">
                                 Increment
                             </button>
-                            <button type="button" class="btn btn-block bg-gradient-danger" v-on:click="remove()">
+                            <button type="button" class="btn btn-block bg-gradient-danger" v-on:click="decrement()">
                                 Decrement
                             </button>
                         </div>
                     </div>
                     <!-- /.row -->
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">DataTable with default features</h3>
+                            </div>
+                            <!-- /.card-header -->
+                            <div class="card-body">
+                                <table id="example1" class="table table-bordered table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>
+                                                <div class="custom-control custom-checkbox">
+                                                    <input class="custom-control-input custom-control-input-success custom-control-input-outline" type="checkbox" id="customCheckbox5" v-on:change="toggleCheckboxes" v-model="selectAll">
+                                                    <label for="customCheckbox5" class="custom-control-label">Id</label>
+                                                </div>
+                                            </th>
+                                            <th>Username</th>
+                                            <th>Password</th>
+                                            <th>User Type</th>
+                                            <th></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr v-for="item in data" v-bind:key="item.id" :id="'row' + item.id">
+                                            <td>
+                                                <div class="custom-control custom-checkbox">
+                                                    <input class="custom-control-input custom-control-input-success" type="checkbox" :id="'chk' + item.id" v-model="selectAll">
+                                                    <label :for="'chk' + item.id" class="custom-control-label">{{ item.id }}</label>
+                                                </div>
+                                            </td>
+                                            <td :id="'row' + item.id + 'username'">{{ item.username }}</td>
+                                            <td :id="'row' + item.id + 'password'">{{ item.password }}</td>
+                                            <td :id="'row' + item.id + 'usertype'">{{ (item.usertype == 1) ? ("Admin") : ("User") }}</td>
+                                            <td>
+                                                <button class="btn btn-app bg-success toastrDefaultSuccess" v-on:click="edit(item.id)">
+                                                    <i class="fas fa-edit"></i><strong>Edit </strong>
+                                                </button>
+                                                <button class="btn btn-app bg-danger" v-on:click="remove(item.id)">
+                                                    <i class="fas fa-trash"></i><strong>Delete </strong>
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </section>
         </div>
@@ -96,14 +145,18 @@
 import headerFile from "./master/header-master.vue";
 import sidebarFile from "./master/sidebar-master.vue";
 import footerFile from "./master/footer-master.vue";
+import axios from 'axios';
 
 export default {
     name: "indexFile",
     data() {
         return {
             count: 0,
+            data: [],
             Time: "09:30:00",
             time: new Date().toLocaleTimeString(),
+            selectAll: false,
+            checkedItems: []
         };
     },
     mounted() {
@@ -133,12 +186,47 @@ export default {
         }, 1);
     },
     methods: {
-        add() {
+        increment() {
             this.count++;
         },
-        remove() {
+        decrement() {
             this.count != 0 ? this.count-- : this.count;
         },
+        toggleCheckboxes() {
+            if (this.selectAll) {
+                this.checkedItems = this.data.map(item => item.id);
+            } else {
+                this.checkedItems = [];
+            }
+        },
+        loadData() {
+            axios.get('http://127.0.0.1:8000/api/data/getUser')
+                .then(response => {
+                    console.log(response.data);
+                    this.data = response.data;
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        },
+        edit(id) {
+            alert("Selected Row Id Is : row" + id);
+        },
+        remove(id) {
+            axios.post('http://127.0.0.1:8000/api/data/deleteUser', { id: id })
+                .then(response => {
+                    console.log(response);
+
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+            // alert("User Deleted Successfully!");
+            this.loadData();
+        },
+    },
+    beforeMount() {
+        this.loadData();
     },
     components: {
         headerFile,
