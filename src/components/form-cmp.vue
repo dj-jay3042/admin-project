8fcfb04a2a0efbe35658c63d533f1237f1d971d9
dj-jay3042 <cjay3042002@gmail.com>
2023-06-16 15:30:39 +0530

    16/06/2023

--- src/components/form-cmp.vue
+++ src/components/form-cmp.vue
@@ -10,7 +10,7 @@
                             <!-- general form elements -->
                             <div class="card card-primary">
                                 <div class="card-header">
-                                    <h3 class="card-title">Quick Example</h3>
+                                    <h3 class="card-title">Add New User</h3>
                                 </div>
                                 <!-- /.card-header -->
                                 <!-- form start -->
